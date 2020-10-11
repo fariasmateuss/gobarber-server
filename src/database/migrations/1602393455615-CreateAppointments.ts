95c41c5fff0f4d573a4e70cda632f61195cd9f31
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-10-11 17:14:39 -0400

    Connect database with typeorm

--- src/database/migrations/1602393455615-CreateAppointments.ts
+++ src/database/migrations/1602393455615-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1602393455615
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,36 +1,14 @@
-import { isEqual } from 'date-fns';
-import Appoitment from '../models/Appointment';
+import { EntityRepository, Repository } from 'typeorm';
+import Appointment from '../models/Appointment';
 
-interface CreateAppointmentDTO {
-  provider: string;
-  date: Date;
-}
-
-class AppointmentsRepository {
-  private appointments: Appoitment[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
-  public all(): Appoitment[] {
-    return this.appointments;
-  }
-
-  public findByDate(date: Date): Appoitment | null {
-    const findAppointment = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
-    );
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: { date },
+    });
 
     return findAppointment || null;
-  }
-
-  public create({ provider, date }: CreateAppointmentDTO): Appoitment {
-    const appointment = new Appoitment({ provider, date });
-
-    this.appointments.push(appointment);
-
-    return appointment;
   }
 }
 
