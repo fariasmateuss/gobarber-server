963d76128c2b00167187db3095999295be927185
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-10-19 23:39:47 -0400

    Serve avatar in static file

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import 'reflect-metadata';
+
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
+
 import './database';
 
 const app = express();
 
+const port = 3333;
+
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333');
+app.listen(port, () => {
+  console.log(`🚀 Server started on port ${port}`);
 });
